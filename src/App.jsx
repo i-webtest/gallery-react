a8c9eab3e6a402229c6311e442624938d7cd00df
i-webtest <stasbass80@gmail.com>
2025-05-23 21:35:54 +0300

    Created Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from './views/Header/Header';
+import { Main } from './views/Main/Main';
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 };
