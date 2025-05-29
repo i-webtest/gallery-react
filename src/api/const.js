94a27cce4b520d0b6a9639ffa13c4027bfd181a0
i-webtest <stasbass80@gmail.com>
2025-05-29 22:25:02 +0300

    Fix commit REDIRECT_URI

--- src/api/const.js
+++ src/api/const.js
@@ -3,6 +3,6 @@ export const API_URL_TOKEN = 'https://unsplash.com/oauth/token';
 export const API_URL_AUTH = 'https://unsplash.com/oauth/authorize';
 export const ACCESS_KEY = 'x1XctRt7lAFZ6dzvX9FQrIqdcXbGMx9FlubAb6hFN78';
 export const SECRET_KEY = '1lXYjBVFkDcS3T8udAik8GZpYI5W2V8h10X32oeij4c';
-export const REDIRECT_URI = 'http://localhost:5173';
+export const REDIRECT_URI = 'https://gallery-react-snowy.vercel.app';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
